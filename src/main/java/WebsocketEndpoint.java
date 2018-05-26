3992625ea1b3279e4a7c1b81d45cb1d1ac80d093
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-05-26 21:53:21 +0200

    Implmenting broadcast, onClose, onOpen and onMessage

--- src/main/java/WebsocketEndpoint.java
+++ src/main/java/WebsocketEndpoint.java
@@ -1,33 +1,69 @@
 
 
 
-import com.sun.xml.internal.ws.wsdl.writer.document.Message;
+
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @ServerEndpoint("/chat")
 public class WebsocketEndpoint{
 
+    private Session session;
+    private static Set<WebsocketEndpoint> chatEndpoints = new CopyOnWriteArraySet<WebsocketEndpoint>();
+    private static HashMap<String, String> users = new HashMap<String, String>();
+
     @OnOpen
-    public void onOpen(Session session) throws IOException {
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
         // Get session and Websocket connection
+        this.session = session;
+        chatEndpoints.add(this);
+        users.put(session.getId(), username);
+
+        Message message = new Message();
+        message.setFrom(username);
+        message.setContent("Connected!");
+        broadcast(message);
     }
 
     @OnMessage
-    public void onMessage(Session session, Message message) throws IOException {
-        // Hnadles new messages
+    public void onMessage(Session session, Message message) throws IOException, EncodeException {
+        // Handles new messages
+        message.setFrom(users.get(session.getId()));
+        broadcast(message);
     }
 
     @OnClose
-    public void onClose(Session session) throws IOException {
+    public void onClose(Session session) throws IOException, EncodeException {
         // Websocket connection close
+        chatEndpoints.remove(this);
+        Message message = new Message();
+        message.setFrom(users.get(session.getId()));
+        message.setContent("Disconnected!");
+        broadcast(message);
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
         // Do error handling
+        
+    }
+
+    private static void broadcast(Message message) throws  IOException, EncodeException {
+        chatEndpoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote().sendObject(message);
+                } catch (IOException | EncodeException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
     }
 
 }
