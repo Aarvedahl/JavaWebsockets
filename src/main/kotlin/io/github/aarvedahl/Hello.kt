6033779e342f5f5990d50cb44b8d04c8eae6d8e9
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-05-31 19:28:47 +0200

    Finding train based on id

--- src/main/kotlin/io/github/aarvedahl/Hello.kt
+++ src/main/kotlin/io/github/aarvedahl/Hello.kt
@@ -1,20 +1,38 @@
 package io.github.aarvedahl
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-data class Train(val trainID: Int, val name: String, val capacity: Int)
+data class Train(val trainID: Int, val destination: String, val capacity: Int)
 
-data class Ticket(val train: Int, val timeToDest: Double, val price: String)
+data class Ticket(val ticketID: Int, val train: Int, val timeToDest: Double, val price: String)
+
+data class Person(val name: String, val tickets: List<Int>)
+
 
 @SpringBootApplication
 open class Application
 
+val tickets = mapOf(
+        Pair("Alex", 2),
+        Pair("Göran", 1),
+        Pair("Sven", 3)
+        )
+
+val trainDestination = setOf<Train>(
+        Train(2, "Stockholm", 200) ,
+        Train(3, "Malmö", 200)
+)
+
+
+fun buyTicket(name: String, destination: String, trains: Set<Train>) {
+    val train = trains.filter { it.destination == destination }
+    print(train.get(0).trainID)
+}
+
 
 fun main(args: Array<String>) {
     println("Hello, World")
-    val tran = Train(3, "OEXP", 120)
-    print(tran.capacity)
+    buyTicket("Alex", "Stockholm", trainDestination)
     //SpringApplication.run(Application::class.java, *args)
 
 }
