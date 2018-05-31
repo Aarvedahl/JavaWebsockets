4aeebbf1e8bf672724b2b7e0212d85cc2ca42577
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-05-31 13:55:34 +0200

    Attemping to add spring

--- src/main/kotlin/io/github/aarvedahl/Hello.kt
+++ src/main/kotlin/io/github/aarvedahl/Hello.kt
@@ -1,11 +1,21 @@
 package io.github.aarvedahl
 
-class Train(val trainID: Int, val name: String, val capacity: String)
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-class Ticket(val train: Int, val timeToDest: Double, val price: String)
+data class Train(val trainID: Int, val name: String, val capacity: Int)
+
+data class Ticket(val train: Int, val timeToDest: Double, val price: String)
+
+@SpringBootApplication
+open class Application
 
 
 fun main(args: Array<String>) {
     println("Hello, World")
+    val tran = Train(3, "OEXP", 120)
+    print(tran.capacity)
+    //SpringApplication.run(Application::class.java, *args)
+
 }
 
