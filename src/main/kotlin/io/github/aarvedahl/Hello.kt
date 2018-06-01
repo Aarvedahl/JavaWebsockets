f415ece626b1868c6d1432c668057720c413dec9
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-06-01 19:45:09 +0200

    Buying tickets works

--- src/main/kotlin/io/github/aarvedahl/Hello.kt
+++ src/main/kotlin/io/github/aarvedahl/Hello.kt
@@ -1,37 +1,50 @@
 package io.github.aarvedahl
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.collections.ArrayList
+
 
 data class Train(val trainID: Int, val destination: String, val capacity: Int)
 
-data class Ticket(val ticketID: Int, val train: Int, val timeToDest: Double, val price: String)
+data class Ticket(val ticketID: Int, val train: Int, val timeToDest: Int, val price: Int)
 
-data class Person(val name: String, val tickets: List<Int>)
+data class Person(val name: String, val tickets: ArrayList<Ticket>)
+
+fun Random.nextInt(range: IntRange): Int {
+    return range.start + nextInt(range.last - range.start)
+}
 
 
-@SpringBootApplication
-open class Application
-
-val tickets = mapOf(
-        Pair("Alex", 2),
-        Pair("Göran", 1),
-        Pair("Sven", 3)
-        )
 
 val trainDestination = setOf<Train>(
         Train(2, "Stockholm", 200) ,
         Train(3, "Malmö", 200)
 )
 
+val tickets = ArrayList<Ticket>()
+val people = setOf<Person>(
+        Person("Alex", ArrayList())
+)
 
 fun buyTicket(name: String, destination: String, trains: Set<Train>) {
+    val random = Random()
     val train = trains.filter { it.destination == destination }
-    print(train.get(0).trainID)
+    val timeToDest = random.nextInt(15..42)
+    val price = random.nextInt(25..60)
+    if(train.size > 0) {
+        val ticket = Ticket(tickets.size +1, train.get(0).trainID, timeToDest, price)
+        val persons = people.filter { it.name == name }
+        if(persons.size > 0) {
+            persons.get(0).tickets.add(ticket)
+            println("Person " + persons.get(0).name + " bought a ticket to " + train.get(0).destination)
+        }
+    }
 }
 
 
 fun main(args: Array<String>) {
-    println("Hello, World")
     buyTicket("Alex", "Stockholm", trainDestination)
     //SpringApplication.run(Application::class.java, *args)
 
